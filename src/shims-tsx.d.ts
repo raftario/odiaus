e3849c06570fabc1e51001ceb2dbdffa0ca48381
Raphaël Thériault <raphael_theriault@outlook.com>
2019-07-01 15:32:34 -0400

    Format vue-cli definition files

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -1,13 +1,18 @@
-import Vue, { VNode } from 'vue';
+import Vue, { VNode } from 'vue'
 
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
+    // tslint:disable interface-name
     interface Element extends VNode {}
+
     // tslint:disable no-empty-interface
+    // tslint:disable interface-name
     interface ElementClass extends Vue {}
+
+    // tslint:disable interface-name
     interface IntrinsicElements {
-      [elem: string]: any;
+      [elem: string]: any
     }
   }
 }
